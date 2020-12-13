6aead82564ed7b9c19ca58bb9cb4019ee072c8db
MrCichy94 <przemyslav.cichy@gmail.com>
2020-12-13 21:21:11 +0100

    Day_5_C1_AddASYNCandMULTIThreadsWithTimers

--- main.cpp
+++ main.cpp
@@ -9,9 +9,11 @@
 
 int main() {
 
-    std::cout << "Threads: " << std::thread::hardware_concurrency() << std::endl;
+    std::cout << "DEBUG MSG: Threads: " << std::thread::hardware_concurrency() << std::endl;
     std::thread::id mainThread = std::this_thread::get_id();
-    std::cout << "Main thread id: " << mainThread << std::endl;
+    std::cout << "DEBUG MSG: Main thread id: " << mainThread << std::endl;
+
+    /* -------!!!------- TRY FUN WITH 1 THREAD RIDE -------!!!------- */
 
     auto startTime = std::chrono::high_resolution_clock::now();
 
@@ -38,7 +40,7 @@ int main() {
 
     std::cout << "Processing (creating ratings) time: " << proccessTime3.count() << " s" << std::endl;
 
-    /* -------!!!------- TRY FUN WITH MULTI -------!!!------- */
+    /* -------!!!------- TRY FUN WITH ASYNC -------!!!------- */
 
     auto threadOne = std::async(std::launch::async, URLCreator::createFullVideoURL, videos);
     auto threadTwo = std::async(std::launch::async, PercentageCounter::countVotesRating, videos);
@@ -50,12 +52,28 @@ int main() {
     threadTwo.get();
     auto finishTime5 = std::chrono::high_resolution_clock::now();
 
+    /* -------!!!------- TRY FUN WITH MULTI -------!!!------- */
+
+    std::thread first(URLCreator::createFullVideoURL, videos);
+    std::thread second(PercentageCounter::countVotesRating, videos);
+
+    std::cout << "----------------------------------------------" << std::endl;
+
+    auto finishTime6 = std::chrono::high_resolution_clock::now();
+    first.join();
+    second.join();
+    auto finishTime7 = std::chrono::high_resolution_clock::now();
+
     std::chrono::duration<double> proccessTime4 = finishTime5 - finishTime4;
+    std::chrono::duration<double> proccessTime5 = finishTime7 - finishTime6;
 
     std::cout << "----------------------------------------------" << std::endl;
     std::cout << "Processing (NORMAL) time: " << proccessTime3.count() + proccessTime2.count() << " s" << std::endl;
-    std::cout << "Processing (MULTI) time: " << proccessTime4.count() << " s" << std::endl;
-    std::cout << "2 THREADs WAS FASTER THAN 1 ABOUT: " << (proccessTime2.count()+proccessTime3.count())-proccessTime4.count() << " s" << std::endl;
+    std::cout << "Processing (ASYNC) time: " << proccessTime4.count() << " s" << std::endl;
+    std::cout << "Processing (MULTI) time: " << proccessTime5.count() << " s" << std::endl;
+    std::cout << "2 THREADs ASYNC WAS FASTER THAN 1 ABOUT: " << (proccessTime2.count()+proccessTime3.count())-proccessTime4.count() << " s" << std::endl;
+    std::cout << "2 THREADs MULTI WAS FASTER THAN 1 ABOUT: " << (proccessTime2.count()+proccessTime3.count())-proccessTime5.count() << " s" << std::endl;
+    std::cout << "ASYNC (1) vs MULTI (2): " << proccessTime4.count() - proccessTime5.count() << std::endl;
 
     return 0;
 }
